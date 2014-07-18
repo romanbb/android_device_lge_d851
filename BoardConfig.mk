600d5157d5b8c71f0479b577fce59b1c68ec66a2
Richard Ross <toastcfh@gmail.com>
2014-07-18 10:27:07 -0400

    initial device tree for lge d851

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,18 +5,46 @@ USE_CAMERA_STUB := true
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_BOARD_PLATFORM := msm8974
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := krait
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+# Krait optimizations
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_KRAIT_BIONIC_PLDOFFS := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH := 10
+TARGET_KRAIT_BIONIC_BBTHRESH := 64
+TARGET_KRAIT_BIONIC_PLDSIZE := 64
+
+TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := d851
 
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g3 user_debug=31 msm_rtb.filter=0x0
-BOARD_KERNEL_BASE := 0x00000000
+
+# kernel cmdline for fastboot booting the image in testing
+# BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g3 user_debug=31 msm_rtb.filter=0x0 LGD851 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_lgit_sic_mipi0_video:1:qcom,mdss_dsi_lgit_sic_mipi1_video uart_console lge.rev=rev_10 lge.bootreason=0x0 lge.hreset=off gpt lge.kcal batt.soc=50 maxcpus=4 kswitch vmalloc=600m
+
+BOARD_KERNEL_BASE := 0x0000000
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x2000000 --tags_offset 0x1e00000
+
+TARGET_KERNEL_SOURCE := kernel/lge/d851
+TARGET_KERNEL_CONFIG := g3-tmo_us-perf_defconfig
+
+# apparently this dont work but should ;x
+# TARGET_KERNEL_APPEND_DTB := true
+# use this instead
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+
+# define dts directory for dtb
+DTS_TARGET := msm8974pro-g3-tmo_us
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
@@ -25,6 +53,7 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/lge/d851/kernel
-
+# recovery stuffs
+TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_HAS_NO_SELECT_BUTTON := true
+RECOVERY_VARIANT := cm
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo cm_d851-eng
+add_lunch_combo cm_d851-user
+
