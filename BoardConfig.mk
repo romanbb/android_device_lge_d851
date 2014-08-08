ceb30bacc6b1a4d2190e17cf74eab5432819f604
Roman Birg <roman@cyngn.com>
2014-08-08 13:45:44 -0700

    remove prebuilt kernel
    Change-Id: Ibcdfcf45510b16b03718991bbf1af70094d52dd6
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,23 +27,24 @@ TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := d851
 
 # not sure why just these cmds dont work
-# BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g3 user_debug=31 msm_rtb.filter=0x0
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g3 user_debug=31 msm_rtb.filter=0x0
+
 # kernel cmdline for fastboot booting the image in testing
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g3 user_debug=31 msm_rtb.filter=0x0 LGD851 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_lgit_sic_mipi0_video:1:qcom,mdss_dsi_lgit_sic_mipi1_video uart_console lge.rev=rev_10 lge.bootreason=0x0 lge.hreset=off gpt lge.kcal batt.soc=50 maxcpus=4 kswitch vmalloc=600m
+#BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=g3 user_debug=31 msm_rtb.filter=0x0 LGD851 mdss_mdp.panel=1:dsi:0:qcom,mdss_dsi_lgit_sic_mipi0_video:1:qcom,mdss_dsi_lgit_sic_mipi1_video uart_console lge.rev=rev_10 lge.bootreason=0x0 lge.hreset=off gpt lge.kcal batt.soc=50 maxcpus=4 kswitch vmalloc=600m
 
 BOARD_KERNEL_BASE := 0x0000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x0008000 --ramdisk_offset 0x2000000 --second_offset 0x0f00000 --tags_offset 0x0000100
 
 # prebuilt kernel
-TARGET_PREBUILT_KERNEL := device/lge/d851/kernel
+#TARGET_PREBUILT_KERNEL := device/lge/d851/kernel
 # else uncomment below to build from sauce
-# TARGET_KERNEL_SOURCE := kernel/lge/d851
-# TARGET_KERNEL_CONFIG := cyanogen_d851_defconfig
+TARGET_KERNEL_SOURCE := kernel/lge/d851
+TARGET_KERNEL_CONFIG := cyanogen_d851_defconfig
 # apparently this dont work but should ;x
 # TARGET_KERNEL_APPEND_DTB := true
 # use this instead
-# BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 14485760
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16485760
